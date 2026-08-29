4b98e13a462f1de7080abd9d3414ebcf44be39a2
Mohamed Sayed <124086059+mohamedsayed-1@users.noreply.github.com>
2026-08-29 10:27:04 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medical_Laboratory_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# medical-laboratory-management-system
+Medical Laboratory Management System built with ASP.NET Core MVC and Entity Framework Core.
